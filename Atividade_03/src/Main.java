df78aa5f1db50c3ed1d5b3593ed631ee20105822
Jpsoaresfig <jpbusinesss@hotmail.com>
2024-03-27 19:36:46 -0300

    inclusão atividade 2 e 3 do curso da udemy

---
+++ Atividade_03/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // Questão 1
+        int a = 10;
+        int b = 30;
+        int soma = a + b;
+        System.out.println(soma);
+
+        //Questão 2
+        int c = -30;
+        int d = 10;
+        int soma2 = c + d;
+        System.out.println(soma2);
+
+        //Questão 3
+        int e = 0;
+        int f = 0;
+        int soma3 = e + f;
+        System.out.println(soma3);
+
+
+    }
+}
